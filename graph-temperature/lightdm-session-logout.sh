432625c5af81ad7c2d6c71d274a4afcc9a6d308c
Thomas Boutry <thomas.boutry@x3rus.com>
2015-05-27 16:25:25 -0400

    Ajout du mode debug car je n'ai pas de bon resultat lors de l'execution

--- graph-temperature/lightdm-session-logout.sh
+++ graph-temperature/lightdm-session-logout.sh
@@ -14,6 +14,8 @@
 # Variables #
 #############
 APP_TO_KILL="graph-temp.sh"
+DEBUG=1
+LOG_DEBUG="/home/xerus/temp_graph/log_kill.log"
 
 
 ########
@@ -23,7 +25,18 @@ APP_TO_KILL="graph-temp.sh"
 # get PID of the app
 PID_APP=$(ps aux | grep $APP_TO_KILL | grep -v grep | tr -s " " | cut -d " " -f 2)
 
+if [ "$DEBUG" = "1" ] ; then
+    echo "---------------------------" >> $LOG_DEBUG 
+    date >> $LOG_DEBUG 
+    ps aux | grep $APP_TO_KILL >> $LOG_DEBUG 
+fi
+
 # If I get a PID send the signal TERM to the app
 if [ ! -z $PID_APP ] ; then
-    kill $PID_APP
+    kill $PID_APP &>>$LOG_DEBUG
+
+    if [ "$DEBUG" = "1" ] ; then
+        ps aux | grep $APP_TO_KILL >> $LOG_DEBUG 
+        echo "----------END------------" >> $LOG_DEBUG    
+    fi
 fi
